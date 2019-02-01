cc8383c2d06a24e792623bda7156cdf32656a7eb
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-02-01 16:56:36 +0100

    projects json creation to simulate API call, tingleJS and types added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+
+import * as Isotope from 'isotope-layout';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    title = 'ng-portfolio';
+export class AppComponent implements OnInit, AfterViewInit {
+    @ViewChild('isotopeContainer') element: ElementRef;
+
+    public isotopeSuit: Isotope;
+    public isotopeElement: HTMLElement;
+
+    constructor(private renderer: Renderer2) {}
+
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {
+        this.isotopeElement = this.element.nativeElement;
+
+        this.isotopeSuit = new Isotope(this.renderer.selectRootElement(this.isotopeElement), {
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows',
+        });
+    }
 }
