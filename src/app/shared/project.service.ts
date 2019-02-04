73152c39c17306564a784074f561788b95e9df00
Osman Fikret Ceylan <ofcyln@gmail.com>
2019-02-04 09:58:55 +0100

    font library implementation, UI implementations

--- src/app/shared/project.service.ts
+++ src/app/shared/project.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment.prod';
+
 export interface Project {
     type: string;
     nameEN: string;
@@ -17,6 +19,6 @@ export class ProjectService {
     constructor(private http: HttpClient) {}
 
     public getProjects(): Observable<Project[]> {
-        return this.http.get<Project[]>('../../assets/api/projects.json');
+        return this.http.get<Project[]>(environment.projectsUrl);
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+    production: true,
+    projectsUrl: '../../assets/api/projects.json',
 };
